5ceb42a69ef068af628ffd92180fb816a3938833
HELLY2319 <133291487+HELLY2319@users.noreply.github.com>
2023-05-12 13:18:16 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,16 @@
+/********************************************************************************* *  WEB322 – Assignment 1 
+*	I declare that this assignment is my own work in accordance with Seneca  Academic Policy.   
+*	No part of this assignment has been copied manually or electronically from any other source 
+*	(including web sites) or distributed to other students. 
+*  
+*	Name: ______helly patel________________ Student ID: ___175295211___________ Date: _5-12-23_______________ 
+* 
+Online (Cyclic) URL: _________________________________________
+https://calm-blue-greyhound-ring.cyclic.app
+*	______________________________________________________ 
+* 
+********************************************************************************/  
+
 var HTTP_PORT = process.env.PORT || 8080;
 var express = require("express");
 var app = express();
